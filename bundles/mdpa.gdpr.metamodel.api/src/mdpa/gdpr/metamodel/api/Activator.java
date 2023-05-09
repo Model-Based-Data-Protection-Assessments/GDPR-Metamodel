b2a3052e74da6bc3eb206a19f139ec178860c252
Nicolas-Boltz <boltz.nicolas@gmx.de>
2023-05-09 15:49:21 +0200

    Initial API to load and access model elements

---
+++ bundles/mdpa.gdpr.metamodel.api/src/mdpa/gdpr/metamodel/api/Activator.java
@@ -0,0 +1,7 @@
+package mdpa.gdpr.metamodel.api;
+
+import org.eclipse.core.runtime.Plugin;
+
+public class Activator extends Plugin {
+
+}
---
+++ bundles/mdpa.gdpr.metamodel.api/src/mdpa/gdpr/metamodel/api/GDPRMetamodelApi.java
@@ -0,0 +1,182 @@
+package mdpa.gdpr.metamodel.api;
+
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import mdpa.gdpr.metamodel.GDPR.AbstractGDPRElement;
+import mdpa.gdpr.metamodel.GDPR.Controller;
+import mdpa.gdpr.metamodel.GDPR.Data;
+import mdpa.gdpr.metamodel.GDPR.LegalAssessmentFacts;
+import mdpa.gdpr.metamodel.GDPR.LegalBasis;
+import mdpa.gdpr.metamodel.GDPR.NaturalPerson;
+import mdpa.gdpr.metamodel.GDPR.Processing;
+import mdpa.gdpr.metamodel.GDPR.Purpose;
+import mdpa.gdpr.metamodel.GDPR.Role;
+import mdpa.gdpr.metamodel.GDPR.ThirdParty;
+import mdpa.gdpr.metamodel.contextproperties.ContextAnnotation;
+import mdpa.gdpr.metamodel.contextproperties.ContextDependentProperties;
+import mdpa.gdpr.metamodel.contextproperties.Property;
+import mdpa.gdpr.metamodel.contextproperties.PropertyAnnotation;
+import mdpa.gdpr.metamodel.contextproperties.PropertyValue;
+
+import org.eclipse.emf.common.EMFPlugin;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+
+import tools.mdsd.library.standalone.initialization.StandaloneInitializationException;
+import tools.mdsd.library.standalone.initialization.StandaloneInitializerBuilder;
+
+public class GDPRMetamodelApi {
+	private static final String PLUGIN_PATH = "mdpa.gdpr.metamodel.api";
+
+	private ResourceSet resources = new ResourceSetImpl();
+	
+	private LegalAssessmentFacts legalAssessmentFacts = null;
+	private Optional<ContextDependentProperties> optContextDependentProperties = Optional.empty();
+	
+	private Map<String, Processing> id2ProcessingMap = new HashMap<>();
+	private Map<String, Purpose> id2PurposeMap = new HashMap<>();
+	private Map<String, LegalBasis> id2LegalBasisMap = new HashMap<>();
+	private Map<String, Data> id2DataMap = new HashMap<>();
+	private Map<String, NaturalPerson> id2NaturalPersonMap = new HashMap<>();
+	private Map<String, Controller> id2ControllerMap = new HashMap<>();
+	private Map<String, ThirdParty> id2ThirdPartyMap = new HashMap<>();
+	
+	private Map<String, Property> id2Property = new HashMap<>();
+	private Map<String, PropertyValue> id2PropertyValue = new HashMap<>();
+	private Map<String, PropertyAnnotation> id2PropertyAnnotation = new HashMap<>();
+	private Map<String, ContextAnnotation> id2ContextAnnotation = new HashMap<>();
+	private Map<AbstractGDPRElement, List<PropertyAnnotation>> annotatedElement2PropertyAnnotation = new HashMap<>();
+	
+	
+	
+	public void load(String gdprModelPath, Optional<String> contextPropertiesModel) {
+		if(EMFPlugin.IS_ECLIPSE_RUNNING) {
+			initStandalone();
+		}
+		
+		loadGDPRModel(gdprModelPath);
+		if(contextPropertiesModel.isPresent()) {			
+			loadContextPropertiesModel(contextPropertiesModel.get());
+		}
+		
+		resolveResources();
+	}
+	
+	public void initializeMappings() {
+		for(Processing process : this.legalAssessmentFacts.getProcessing()) {
+			this.id2ProcessingMap.put(process.getId(), process);
+		}
+		for(Purpose purpose : this.legalAssessmentFacts.getPurposes()) {
+			this.id2PurposeMap.put(purpose.getId(), purpose);
+		}
+		for(LegalBasis basis : this.legalAssessmentFacts.getLegalBases()) {
+			this.id2LegalBasisMap.put(basis.getId(), basis);
+		}
+		for(Data data : this.legalAssessmentFacts.getData()) {
+			this.id2DataMap.put(data.getId(), data);
+		}
+		for(Role role : this.legalAssessmentFacts.getInvolvedParties()) {
+			if(role instanceof NaturalPerson) {
+				this.id2NaturalPersonMap.put(role.getId(), (NaturalPerson) role);
+			} else if (role instanceof Controller) {
+				this.id2ControllerMap.put(role.getId(), (Controller) role);
+			} else if (role instanceof ThirdParty) {
+				this.id2ThirdPartyMap.put(role.getId(), (ThirdParty) role);
+			} else {
+				// should never occur and is currently not mapped
+			}
+		}
+		
+		if(optContextDependentProperties.isPresent()) {
+			ContextDependentProperties contextDependentProperties = optContextDependentProperties.get();
+			for(Property property : contextDependentProperties.getProperty()) {
+				this.id2Property.put(property.getId(), property);
+				for(PropertyValue propertyValue : property.getPropertyvalue()) {
+					this.id2PropertyValue.put(propertyValue.getId(), propertyValue);
+				}
+			}
+			for(PropertyAnnotation propertyAnnotation : contextDependentProperties.getPropertyannotation()) {
+				this.id2PropertyAnnotation.put(propertyAnnotation.getId(), propertyAnnotation);
+				for(ContextAnnotation contextAnnotation : propertyAnnotation.getContextannotation()) {
+					this.id2ContextAnnotation.put(contextAnnotation.getId(), contextAnnotation);
+				}
+				AbstractGDPRElement annotatedElement = propertyAnnotation.getAnnotatedElement();
+				if(!this.annotatedElement2PropertyAnnotation.containsKey(annotatedElement)) {
+					this.annotatedElement2PropertyAnnotation.put(annotatedElement, new ArrayList<>());
+				}
+				this.annotatedElement2PropertyAnnotation.get(annotatedElement).add(propertyAnnotation);
+			}
+		}
+	}
+	
+	public LegalAssessmentFacts getLegalAssessmentFacts() {
+		return this.legalAssessmentFacts;
+	}
+	
+	public Optional<ContextDependentProperties> getContextDependentProperties() {
+		return this.optContextDependentProperties;
+	}
+		
+	private void loadGDPRModel(String gdprModelPath) {
+		URI gdprModelURI = createRelativePluginURIFromAbsolutePath(gdprModelPath);
+
+		this.legalAssessmentFacts = (LegalAssessmentFacts) this.loadResource(gdprModelURI);		
+	}
+	
+	private void loadContextPropertiesModel(String contextPropertiesModelPath) {
+		URI contextPropertiesModelURI = createRelativePluginURIFromAbsolutePath(contextPropertiesModelPath);
+
+		this.optContextDependentProperties = Optional.of((ContextDependentProperties) this.loadResource(contextPropertiesModelURI));		
+	}
+	
+	private void resolveResources() {
+		List<Resource> loadedResources = null;
+		do {
+			loadedResources = new ArrayList<>(this.resources.getResources());
+			loadedResources.forEach(it->EcoreUtil.resolveAll(it));
+		} while (loadedResources.size() != this.resources.getResources().size());
+	}
+	
+	private EObject loadResource(URI modelURI) {
+		Resource resource = resources.getResource(modelURI, true);
+		if (resource == null) {
+			throw new IllegalArgumentException(String.format("Model with URI %s could not be loaded", modelURI));
+		} else if (resource.getContents().isEmpty()) {
+			throw new IllegalArgumentException(String.format("Model with URI %s is empty", modelURI));
+		}
+		return resource.getContents().get(0);
+	}
+	
+    private boolean initStandalone() {
+        try {
+            StandaloneInitializerBuilder.builder()
+                .registerProjectURI(GDPRMetamodelApi.class, GDPRMetamodelApi.PLUGIN_PATH)
+                .build()
+                .init();
+            return true;
+
+        } catch (StandaloneInitializationException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+	
+    private URI createRelativePluginURIFromProjectPath(String projectName, String relativePath) {
+        String path = Paths.get(projectName, relativePath)
+            .toString();
+        return URI.createPlatformPluginURI(path, false);
+    }
+    
+    private URI createRelativePluginURIFromAbsolutePath(String absolutePath) {
+    	return URI.createPlatformPluginURI(absolutePath, false);
+    }
+}
